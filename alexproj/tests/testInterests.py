f56718ccdc51995526a6c3288d20f8bb94d22ead
Alex Meng <alexmeng99@gmail.com>
2022-08-03 16:56:42 -0700

    Update docker and test prints

--- alexproj/tests/testInterests.py
+++ alexproj/tests/testInterests.py
@@ -11,10 +11,12 @@ class TestInterests(unittest.TestCase):
         
         result = run("Userfile.txt")
         self.assertEqual(result, [('games', 10), ('arena', 6), ('apex', 4), ('various', 4), ('syndicate', 4)])
+        print("Test 1 Succesful")
 
     def test_InterestReturn2(self):
         
         result = run("Alex.txt")
         self.assertEqual(result, [('jesse', 12), ('ivor', 6), ('group', 6), ('wither', 5), ('portal', 4)])
+        print("Test 2 Succesful")
 
 
